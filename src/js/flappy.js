b33aa39aa011e7a65c012bda1e7d12d97930a6a5
Gabriel <gabriel52.alves@gmail.com>
2023-04-30 13:04:40 -0300

    footer

--- src/js/flappy.js
+++ src/js/flappy.js
@@ -69,7 +69,7 @@ function Passaro(alturaJogo) {
     let voando = false
     
     this.elemento = novoElemento('img', 'passaro')
-    this.elemento.src = '/src/assets/images/passaro.png'
+    this.elemento.src = '/flappy-bird-js/src/assets/images/passaro.png'
 
     this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
     this.setY = y => this.elemento.style.bottom = `${y}px`
